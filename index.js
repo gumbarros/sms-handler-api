1e05389722d73fc725920bd0a2dc032cf08d0c93
gumbarros <gustavomauriciodebarros@gmail.com>
2021-02-02 20:50:52 -0300

    SMS DB

--- index.js
+++ index.js
@@ -2,35 +2,55 @@ const express = require("express");
 const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser')
-
+const db = require('better-sqlite3')('sms.db', { verbose: console.log });
 app.use(bodyParser.json());
 
 app.get("/", function(req,res){
     res.send("API Online...");
 });
 
-app.route("/sms")
+app.route("/sms/:phone?")
 .post(function(req,res){
 
-    const sms = req.body.sms
+    const sms = {
+      phone: req.body.phone,  
+      content: req.body.content,
+      date: req.body.date
+    }
 
-    fs.writeFile("sms.txt", sms, function(err){
-        if (err) res.send(err);
-        return res.send("SMS gravado.")
+    const statement = db.prepare('INSERT INTO TB_SMS (SMS_PHONE,SMS_CONTENT,SMS_DATE) VALUES(@phone,@content,@date)');
+
+    const insertSMS = db.transaction((sms) => {
+       statement.run(sms);
     });
+
+    insertSMS(sms);
+
+    return res.send("SMS gravado com sucesso no DB.")
 })
 .get(function(req,res){
-    fs.readFile('sms.txt', function(err, data) {
-        if (err) res.send(err);
-        const sms = data.toString()
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        return res.end(JSON.stringify({ sms:sms }));
-      });
+
+    const phone = req.params.phone;
+
+    let statement
+
+    if (phone != null){
+        statement = db.prepare('SELECT * FROM TB_SMS WHERE SMS_PHONE =' + phone);
+    }
+
+    else{
+        statement = db.prepare('SELECT * FROM TB_SMS');
+    }
+
+    const sms = statement.all()
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    return res.end(JSON.stringify(sms));
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
     console.log(port + "!");
-    
 });
